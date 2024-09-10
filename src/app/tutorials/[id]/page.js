426d661add9405d4f52bde69d73f1fe93bb41ebb
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-09-10 20:48:46 +0530

    UI

--- src/app/tutorials/[id]/page.js
+++ src/app/tutorials/[id]/page.js
@@ -11,6 +11,11 @@ export default function SingleTutorialPage({ params }) {
 
     const [tutorial, setTutorial] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [isModalOPen, setIsModalOpen] = useState(true);
+
+    const modalHandler = () => {
+        setIsModalOpen(!isModalOPen)
+    }
 
     useEffect(() => {
         if (id) { 
@@ -47,9 +52,22 @@ export default function SingleTutorialPage({ params }) {
               ) : (
                   <p>Invalid video URL.</p>
               )}
-              <div className='w-[50%] flex justify-center items-center gap-7 h-full'>
-              <Button>Add Notes➕</Button>
+              <div className='w-[50%] flex flex-col space-y-6 justify-center items-center h-full'>
+              <div className='flex gap-7'>
+              <Button  onClick={modalHandler}>Add Notes➕</Button>
               <Button>Completed✅</Button>
+              </div>
+
+              { isModalOPen === true &&
+                <div className='flex flex-col justify-center items-center space-y-4 bg-purple-600 bg-opacity-55 px-6 py-3 rounded-xl border border-gray-300'>
+                   <div className='flex justify-between gap-2'>
+                   <Button>Edit⚙️</Button>
+                   <Button>Remove Notes🗑️</Button>
+                   <p onClick={() => setIsModalOpen(false)} className='cursor-pointer p-2 rounded-full bg-white text-[12px]'>❌</p>
+                   </div>
+                   <textarea placeholder='add your notes here...' className='dark text-black border border-violet-300 rounded-xl px-6 py-3' value={tutorial?.notes}/>
+                </div>
+              }
               </div>
             </div>
         </div>
