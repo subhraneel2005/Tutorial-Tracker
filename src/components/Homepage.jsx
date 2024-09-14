1eb4f679fe4ce7f86516b4f3dd9b91dcfcb66f8d
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-09-14 22:07:17 +0530

    link added

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,6 +1,10 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 function Homepage() {
+  const router = useRouter()
   return (
     <div className='min-h-screen w-full flex justify-center md:px-12 items-center'>
         <div className='mobileBox md:hidden flex'></div>
@@ -11,7 +15,7 @@ function Homepage() {
                 <p className='text-[0.8rem] md:text-[1.3rem] text-[#F5F3F4]'>Be more productive. Earn </p>
                 <p className='text-[0.8rem] md:text-[1.3rem] text-[#F5F3F4]'>exciting rewards.</p>
             </div>
-            <button className='md:btn mobileBtn'>Get Started</button>
+            <button className='md:btn mobileBtn' onClick={() => router.push('/tutorials')}>Get Started</button>
         </div>
         <div className='box md:left-[1100px] absolute hidden md:flex'></div>
     </div>
